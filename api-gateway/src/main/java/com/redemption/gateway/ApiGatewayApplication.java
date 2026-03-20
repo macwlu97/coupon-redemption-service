0c8ff9dace94188b72ee8286f626bdc846c278d1
macwlu97 <maciej.wnuklipinski@gmail.com>
2026-03-20 22:08:00 +0100

    MWL - docs: Add comments to ApiGatewayApplication

--- api-gateway/src/main/java/com/redemption/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/redemption/gateway/ApiGatewayApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Single entry point for the system.
+ * Routes traffic to appropriate microservices using Eureka service names.
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
