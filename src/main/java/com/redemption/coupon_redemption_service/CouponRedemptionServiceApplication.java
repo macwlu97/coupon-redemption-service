0a87ea68ad83a7c37f71a93b0df1e6f5940208c1
macwlu97 <maciej.wnuklipinski@gmail.com>
2026-03-20 17:16:10 +0100

    first commit

---
+++ src/main/java/com/redemption/coupon_redemption_service/CouponRedemptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.redemption.coupon_redemption_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponRedemptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponRedemptionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redemption/coupon_redemption_service/CouponRedemptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redemption.coupon_redemption_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponRedemptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
